e33b4ccbb96397d170122ae99f90d07477e50387
Demo User <demouser@MacBook-Air.local>
2017-08-01 09:40:34 -0400

    finished nutrition

--- Templates/nutrition.js
+++ Templates/nutrition.js
@@ -1,66 +1,66 @@
 function toggleWhenClicked() {
-    $("#stress_tip1P").toggle();
+    $("#nutrition_tip1P").toggle();
 }
 
 function toggleWhenClicked() {
-    $("#stress_tip2P").toggle();
+    $("#nutrition_tip2P").toggle();
 }
 
 function toggleWhenClicked() {
-    $("#stress_tip3P").toggle();
+    $("#nutrition_tip3P").toggle();
 }
 
 function toggleWhenClicked() {
-    $("#stress_tip4P").toggle();
+    $("#nutrition_tip4P").toggle();
 }
 
 function toggleWhenClicked() {
-    $("#stress_tip5P").toggle();
+    $("#nutrition_tip5P").toggle();
 }
 
 function toggleWhenClicked() {
-    $("#stress_tip6P").toggle();
+    $("#nutrition_tip6P").toggle();
 }
 
 function toggleWhenClicked() {
-    $("#stress_tip7P").toggle();
+    $("#nutrition_tip7P").toggle();
 }
 
 function toggleWhenClicked() {
-    $("#stress_tip8P").toggle();
+    $("#nutrition_tip8P").toggle();
 }
 
 function toggleWhenClicked() {
-    $("#stress_tip9P").toggle();
+    $("#nutrition_tip9P").toggle();
 }
 
 function toggleWhenClicked() {
-    $("#stress_tip10P").toggle();
+    $("#nutrition_tip10P").toggle();
 }
 
 
 function setup() {
-  $("#stress_tip1P").hide();
-  $("#stress_tip2P").hide();
-  $("#stress_tip3P").hide();
-  $("#stress_tip4P").hide();
-  $("#stress_tip5P").hide();
-  $("#stress_tip6P").hide();
-  $("#stress_tip7P").hide();
-  $("#stress_tip8P").hide();
-  $("#stress_tip9P").hide();
-  $("#stress_tip10P").hide();
+  $("#nutrition_tip1P").hide();
+  $("#nutrition_tip2P").hide();
+  $("#nutrition_tip3P").hide();
+  $("#nutrition_tip4P").hide();
+  $("#nutrition_tip5P").hide();
+  $("#nutrition_tip6P").hide();
+  $("#nutrition_tip7P").hide();
+  $("#nutrition_tip8P").hide();
+  $("#nutrition_tip9P").hide();
+  $("#nutrition_tip10P").hide();
 
-  $("#stress_tip1").click(toggleWhenClicked);
-  $("#stress_tip2").click(toggleWhenClicked);
-  $("#stress_tip3").click(toggleWhenClicked);
-  $("#stress_tip4").click(toggleWhenClicked);
-  $("#stress_tip5").click(toggleWhenClicked);
-  $("#stress_tip6").click(toggleWhenClicked);
-  $("#stress_tip7").click(toggleWhenClicked);
-  $("#stress_tip8").click(toggleWhenClicked);
-  $("#stress_tip9").click(toggleWhenClicked);
-  $("#stress_tip10").click(toggleWhenClicked);
+  $("#nutrition_tip1").click(toggleWhenClicked);
+  $("#nutrition_tip2").click(toggleWhenClicked);
+  $("#nutrition_tip3").click(toggleWhenClicked);
+  $("#nutrition_tip4").click(toggleWhenClicked);
+  $("#nutrition_tip5").click(toggleWhenClicked);
+  $("#nutrition_tip6").click(toggleWhenClicked);
+  $("#nutrition_tip7").click(toggleWhenClicked);
+  $("#nutrition_tip8").click(toggleWhenClicked);
+  $("#nutrition_tip9").click(toggleWhenClicked);
+  $("#nutrition_tip10").click(toggleWhenClicked);
 
 }
 
