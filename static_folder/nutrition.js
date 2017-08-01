f0470ebe102a21354bcddebe1bffb117aa630bb6
Demo User <demouser@MacBook-Air.local>
2017-08-01 10:39:21 -0400

    gjadg

--- static_folder/nutrition.js
+++ static_folder/nutrition.js
@@ -2,39 +2,39 @@ function toggleWhenClicked() {
     $("#nutrition_tip1P").toggle();
 }
 
-function toggleWhenClicked() {
+function toggleWhenClicked2() {
     $("#nutrition_tip2P").toggle();
 }
 
-function toggleWhenClicked() {
+function toggleWhenClicked3() {
     $("#nutrition_tip3P").toggle();
 }
 
-function toggleWhenClicked() {
+function toggleWhenClicked4() {
     $("#nutrition_tip4P").toggle();
 }
 
-function toggleWhenClicked() {
+function toggleWhenClicked5() {
     $("#nutrition_tip5P").toggle();
 }
 
-function toggleWhenClicked() {
+function toggleWhenClicked6() {
     $("#nutrition_tip6P").toggle();
 }
 
-function toggleWhenClicked() {
+function toggleWhenClicked7() {
     $("#nutrition_tip7P").toggle();
 }
 
-function toggleWhenClicked() {
+function toggleWhenClicked8() {
     $("#nutrition_tip8P").toggle();
 }
 
-function toggleWhenClicked() {
+function toggleWhenClicked9() {
     $("#nutrition_tip9P").toggle();
 }
 
-function toggleWhenClicked() {
+function toggleWhenClicked10() {
     $("#nutrition_tip10P").toggle();
 }
 
@@ -52,15 +52,15 @@ function setup() {
   $("#nutrition_tip10P").hide();
 
   $("#nutrition_tip1").click(toggleWhenClicked);
-  $("#nutrition_tip2").click(toggleWhenClicked);
-  $("#nutrition_tip3").click(toggleWhenClicked);
-  $("#nutrition_tip4").click(toggleWhenClicked);
-  $("#nutrition_tip5").click(toggleWhenClicked);
-  $("#nutrition_tip6").click(toggleWhenClicked);
-  $("#nutrition_tip7").click(toggleWhenClicked);
-  $("#nutrition_tip8").click(toggleWhenClicked);
-  $("#nutrition_tip9").click(toggleWhenClicked);
-  $("#nutrition_tip10").click(toggleWhenClicked);
+  $("#nutrition_tip2").click(toggleWhenClicked2);
+  $("#nutrition_tip3").click(toggleWhenClicked3);
+  $("#nutrition_tip4").click(toggleWhenClicked4);
+  $("#nutrition_tip5").click(toggleWhenClicked5);
+  $("#nutrition_tip6").click(toggleWhenClicked6);
+  $("#nutrition_tip7").click(toggleWhenClicked7);
+  $("#nutrition_tip8").click(toggleWhenClicked8);
+  $("#nutrition_tip9").click(toggleWhenClicked9);
+  $("#nutrition_tip10").click(toggleWhenClicked10);
 
 }
 
